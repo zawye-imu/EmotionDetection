729d70b105626763cc76a0a51eb6dc4e312baa89
zawye-imu <53737389+zawye-imu@users.noreply.github.com>
2019-11-28 18:41:56 +0630

    files upload

---
+++ test23(eval_tweets).py
@@ -0,0 +1,86 @@
+import keras
+import pandas as pd
+df=pd.read_csv(r"C:\Users\User\Desktop\PROJECT\Work\Test batches\Test1(3500).csv",encoding="ISO-8859-1")
+
+new_X=list(df['sentence'].values)
+Y=list(df['emotion'].values)
+
+
+
+from import_nltk import t_process
+X=[]
+use_X=[]
+for sen in new_X:
+    tmp=t_process(str(sen))
+    X.extend(tmp)
+    use_X.append(' '.join(tmp))
+    
+    
+
+
+unique_words=list(set(X))
+le=len(unique_words)
+print("Vocab size:::"+str(le))
+
+
+from keras.preprocessing.text import Tokenizer
+tokenizer=Tokenizer(le)
+tokenizer.fit_on_texts(use_X)
+diction=tokenizer.word_index.items()
+
+
+
+
+
+from keras.preprocessing.sequence import pad_sequences
+maxlen=50
+
+
+
+from keras.models import load_model
+model=load_model('m1')
+model.summary()
+
+
+## Needed for testing
+import numpy
+
+
+tmp_ls=[]
+pre_str="I am angry at this bitch"
+new_str=' '.join(t_process(str(pre_str)))
+##print(new_str)
+tmp_ls.append(new_str)
+##print(tmp_ls)
+sq_str=tokenizer.texts_to_sequences(tmp_ls)
+pd_str=pad_sequences(sq_str, padding='post', maxlen=maxlen)
+##print(pd_str)
+Xnew=numpy.array(pd_str)
+Ynew=model.predict_classes(Xnew)
+print("X=%s, Predicted=%s" % (Xnew[0], Ynew[0]))
+
+
+##Testing on JB
+##with open('jb_20.txt') as f:
+##    content=f.readlines()
+
+##Testig on newyork times
+with open('test.txt') as f:
+    content=f.readlines()
+
+for x in content:
+    tmp_ls=[]
+    pre_str=x.strip()
+    new_str=' '.join(t_process(str(pre_str)))
+    ##print(new_str)
+    tmp_ls.append(new_str)
+    ##print(tmp_ls)
+    sq_str=tokenizer.texts_to_sequences(tmp_ls)
+    pd_str=pad_sequences(sq_str, padding='post', maxlen=maxlen)
+    ##print(pd_str)
+    Xnew=numpy.array(pd_str)
+    Ynew=model.predict_classes(Xnew)
+    print("X=%s, Predicted=%s" % (x.strip(), Ynew[0]))
+    
+
+
---
+++ test23(keyword_method).py
@@ -0,0 +1,122 @@
+import pandas as pd
+df=pd.read_csv(r"C:\Users\User\Desktop\PROJECT\Work\DataSets\sen_res.csv",encoding="ISO-8859-1")
+
+
+term=list(df['term'].values)
+score=list(df['score'].values)
+AffectDimension=list(df['AffectDimension'].values)
+
+
+##dictionary generation
+diction=[['term','score','AD']]
+i=0
+for x in term:
+    diction.append([x,score[i],AffectDimension[i]])
+    i=i+1
+
+##print(len(term))
+##print(diction['outraged'])
+
+print(diction[:5])
+print(len(diction))
+
+
+#### Some NLP
+
+def aff_intensity(inp):
+    
+
+    s1="This is the test's sentence? right!!.."
+
+    ##Actual sentence now
+    s1="I am happy and sad at the same time ughhhh"
+
+    
+    s1=inp
+
+
+
+    ##lowercase
+    s1=s1.lower()
+
+    ##removing punctuations
+    import string
+    s1=s1.translate(str.maketrans('', '', string.punctuation))
+
+
+    ##print(s1)
+
+    ##Tokenizing
+    import nltk
+    tokenizer=nltk.tokenize.TreebankWordTokenizer()
+    s1=tokenizer.tokenize(s1)
+
+    s2=[]
+    stemer2=nltk.stem.WordNetLemmatizer()
+    for word in s1:
+        s2.append(stemer2.lemmatize(word))
+
+    from nltk.corpus import stopwords
+    stop_words=set(stopwords.words("english"))
+    filtered_sentence=[w for w in s2 if not w in stop_words]
+
+    
+
+    print(filtered_sentence)
+
+
+    #### Classying emotion
+
+
+    classification=[]
+    for word in filtered_sentence:
+        for row in diction:
+            if row[0] == word:
+                
+                classification.append(row)
+                break
+
+##    print(classification)
+
+
+    ## Post processing
+    final_score=0
+       
+    for rows in classification:
+        if rows[2]=='anger':
+            final_score+=rows[1]
+
+    print("anger score:"+str(final_score))
+
+    final_score=0
+    for rows in classification:
+        if rows[2]=='sadness':
+            final_score+=rows[1]
+
+    print("sadness score:"+str(final_score))
+
+    final_score=0
+    for rows in classification:
+        if rows[2]=='joy':
+            final_score+=rows[1]
+
+    print("joy score:"+str(final_score))
+
+    final_score=0
+    for rows in classification:
+        if rows[2]=='fear':
+            final_score+=rows[1]
+
+    print("fear score:"+str(final_score))
+
+
+
+##Testing the results part
+with open('test.txt') as f:
+    content=f.readlines()
+
+
+for sentence in content:
+    print("\n\n"+sentence)
+    aff_intensity(sentence)
+##aff_intensity("The dogs are happy")
